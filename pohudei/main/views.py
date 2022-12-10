4b89c50ec286c5f7be24ac5387fa70ee9eb9aeda
me <dont@mail.me>
2022-12-11 01:55:38 +0400

    new chart! Eaten and target kcals!!11

--- pohudei/main/views.py
+++ pohudei/main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from datetime import datetime, timedelta
 from .models import *
 import json
+import locale
+locale.setlocale(locale.LC_ALL, "ru_RU.utf8")
 
 
 def home(request):
@@ -95,46 +97,41 @@ def delete_weight(request):
 def diary(request):
     if request.user.is_authenticated:
         user_id = request.user.profile.user_id
-        # print('user_id:', user_id)
         if user_id:
+            eaten = []
+            weights = []
             sum_kcals_and_weight = db_get_everyday_sum_kcals_from_diary(user_id)
-            # for i in sum_kcals_and_weight:
-            #     print(i)
-            init_avg = 7
-            table_length = len(sum_kcals_and_weight)
-            if table_length < 30:
-                init_avg = round(table_length / 4)
-            if init_avg == 0:
-                init_avg = 1
-            start_weight = 0
-            for i in sum_kcals_and_weight[:init_avg]:
-                start_weight += i[2]
-            start_weight = start_weight / init_avg
-            # print('start_weight:', start_weight)
-            # print()
-            end_weight = 0
-            for i in sum_kcals_and_weight[-init_avg:]:
-                end_weight += i[2]
-            end_weight = end_weight / init_avg
-            # print('end_weight:', end_weight)
-            # print()
-            all_eaten = 0
-            for i in sum_kcals_and_weight:
-                all_eaten += int(i[1])
-            # print('all_eaten:', all_eaten)
-            # print()
-            weight_delta = end_weight - start_weight
-            # print('weight_delta:', weight_delta)
-            # print()
-            target_kcals = round((all_eaten - (weight_delta * 7700)) / table_length)
-            # print('target_kcals:', target_kcals)
-            # print()
+            for row in sum_kcals_and_weight:
+                eaten.append(float(row[1]))
+                weights.append(float(row[2]))
+
+            avg_weights = []
+            target_kcals = []
+            for i, row in enumerate(sum_kcals_and_weight):
+                j = 0
+                if i - 6 > 0:
+                    j = i - 6
+                tmp_weights_list = weights[j:i+1]
+                tmp_weights_sum = sum(tmp_weights_list)
+                tmp_avg_weight = round(tmp_weights_sum / len(tmp_weights_list), 2)
+                avg_weights.append(tmp_avg_weight)
+
+                k = 0
+                if i - 30 > 0:
+                    k = i - 30
+                tmp_eaten_list = eaten[k:i+1]
+                tmp_eaten_sum = sum(tmp_eaten_list)
+
+                num_of_days = i - k
+                if num_of_days == 0:
+                    num_of_days = 1
+                tmp_target_kcals = round((tmp_eaten_sum - ((avg_weights[i] - avg_weights[k]) * 7700)) / num_of_days)
+                target_kcals.append(tmp_target_kcals)
+
+            todays_target_kcals = target_kcals[-1]
             today_food = db_get_today_food_from_diary(user_id)
             all_foods = db_get_food_names()
-            # print(all_foods)
-            # for i in today_food:
-            #     print(i)
-            return render(request, 'main/diary.html', {'data': [today_food, target_kcals, all_foods]})
+            return render(request, 'main/diary.html', {'data': [today_food, todays_target_kcals, all_foods]})
         return redirect('home')
     return redirect('login')
 
@@ -149,7 +146,7 @@ def add_food_to_diary(request):
             # print('data:', data)
 
             today = datetime.today()
-            today_str = today.strftime("%Y-%m-%d")  # 07-07-2021
+            today_str = today.strftime("%Y-%m-%d")
             # yesterday = datetime.today() - timedelta(days=1)  # TODO: make food addition for different dates
 
             result = db_add_new_diary_entry(user_id, today_str, data['food_id'], data['food_weight'])
@@ -221,21 +218,65 @@ def stats(request):
     except:
         return redirect('noprofile')
 
-    results = db_get_basic_stats(user_id)
-    dates = []
+    # results = db_get_basic_stats(user_id)
+    # dates = []
+    # weights = []
+    # for i in results[1]:
+    #     dates.append(i[0])
+    #     weights.append(i[1])
+
+    human_dates = []
+    eaten = []
     weights = []
-    for i in results[1]:
-        print(i)
-        dates.append(i[0])
-        weights.append(i[1])
-
     sum_kcals_and_weight = db_get_everyday_sum_kcals_from_diary(user_id)
-    for i in sum_kcals_and_weight:
-        print(i)
+    for row in sum_kcals_and_weight:
+        print(row)
+        human_dates.append(row[0].strftime("%d %b"))
+        eaten.append(float(row[1]))
+        weights.append(float(row[2]))
 
-    # if results[0] == 'success':
-    return render(request, 'main/stats.html', {'data': {'dates': dates, 'weights': weights}})
-    # return render(request, 'main/options.html')
+    avg_weights = []
+    target_kcals = []
+    for i, row in enumerate(sum_kcals_and_weight):
+        j = 0
+        if i - 6 > 0:
+            j = i - 6
+        tmp_weights_list = weights[j:i+1]
+        tmp_weights_sum = sum(tmp_weights_list)
+        tmp_avg_weight = round(tmp_weights_sum / len(tmp_weights_list), 2)
+        avg_weights.append(tmp_avg_weight)
+
+        k = 0
+        if i - 30 > 0:
+            k = i - 30
+        tmp_eaten_list = eaten[k:i+1]
+        tmp_eaten_sum = sum(tmp_eaten_list)
+
+        num_of_days = i - k
+        if num_of_days == 0:
+            num_of_days = 1
+        tmp_target_kcals = round((tmp_eaten_sum - ((avg_weights[i] - avg_weights[k]) * 7700)) / num_of_days)
+        target_kcals.append(tmp_target_kcals)
+
+    prepped_normal_weights = []
+    prepped_average_weights = []
+    prepped_eaten_kcals = []
+    prepped_target_kcals = []
+
+    for i in range(len(human_dates)):
+        prepped_normal_weights.append({'x': human_dates[i], 'y': weights[i]})
+        prepped_average_weights.append({'x': human_dates[i], 'y': avg_weights[i]})
+
+        prepped_eaten_kcals.append({'x': human_dates[i], 'y': eaten[i]})
+        prepped_target_kcals.append({'x': human_dates[i], 'y': target_kcals[i]})
+        # target_kcals_tmp = target_kcals[i]
+        # if target_kcals_tmp > 3000:
+        #     target_kcals_tmp = None
+        # prepped_target_kcals.append({'x': human_dates[i], 'y': target_kcals_tmp})
+
+    return render(request, 'main/stats.html', {'data': {
+        'weights_chart': {'normal': prepped_normal_weights, 'average': prepped_average_weights},
+        'kcals_chart': {'eaten': prepped_eaten_kcals, 'target': prepped_target_kcals}}})
 
 
 ### OPTIONS FNs ###############################################################
