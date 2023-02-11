f33f38f306752d61e8211391792a4a36c643a883
me <dont@mail.me>
2023-02-11 21:11:15 +0400

    deployment

--- pohudei/pohudei-project/settings.py
+++ pohudei/pohudei-project/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('W_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['45.155.120.131', '127.0.0.1']
+ALLOWED_HOSTS = ['45.155.120.131', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
