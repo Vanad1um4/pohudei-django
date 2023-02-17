759e71385fda8913037072827c042e87c32e44a9
vnd <vanad1um3@gmail.com>
2023-02-17 22:00:02 +0400

    minor interface fixes

--- pohudei/pohudei-project/settings.py
+++ pohudei/pohudei-project/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('W_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['45.155.120.131', '127.0.0.1', 'localhost']
 
